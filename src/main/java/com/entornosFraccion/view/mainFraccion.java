89857df0207a44ba85dd45b700c7f7716f718779
codeGraphK <kevinruizflorez@gmail.com>
2020-05-14 02:11:46 +0200

    Implemented MCD

--- src/main/java/com/entornosFraccion/view/mainFraccion.java
+++ src/main/java/com/entornosFraccion/view/mainFraccion.java
@@ -1,9 +1,13 @@
 package com.entornosFraccion.view;
 
+import com.entornosFraccion.controller.Operaciones;
+import com.entornosFraccion.model.Fraccion;
+
 public class mainFraccion {
-    
+
     public static void main(String[] args) {
-        
+        Fraccion frUno = new Fraccion(42, 56);
+        System.out.println(Operaciones.maxComDivisor(frUno));
     }
 
 }
