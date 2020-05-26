e0d50d111a38b87ef87e805b07fd418ea19c76b2
codeGraphK <kevinruizflorez@gmail.com>
2020-05-26 12:29:57 +0200

    Fixed import from model.Fraccion error

--- src/main/java/com/entornosFraccion/controller/Operaciones.java
+++ src/main/java/com/entornosFraccion/controller/Operaciones.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import com.entornosFraccion.model.Fraccion;
+
 public class Operaciones {
 
     public static boolean esPar(int numero) {
